65b57ef53381cb83552b7e1386e44616a65117cd
Durai Pandian <dduraipandian@gmail.com>
2026-04-07 17:51:16 +0530

    removing default context menu and creating single instance app

--- app.go
+++ app.go
@@ -3,6 +3,10 @@ package main
 import (
 	"context"
 	"fmt"
+	"strings"
+
+	"github.com/wailsapp/wails/v2/pkg/options"
+	"github.com/wailsapp/wails/v2/pkg/runtime"
 )
 
 // App struct
@@ -13,6 +17,16 @@ type App struct {
 // NewApp creates a new App application struct
 func NewApp() *App {
 	return &App{}
+}
+
+func (a *App) onSecondInstanceLaunch(secondInstanceData options.SecondInstanceData) {
+	secondInstanceArgs := secondInstanceData.Args
+
+	println("user opened second instance", strings.Join(secondInstanceData.Args, ","))
+	println("user opened second from", secondInstanceData.WorkingDirectory)
+	runtime.WindowUnminimise(a.ctx)
+	runtime.Show(a.ctx)
+	go runtime.EventsEmit(a.ctx, "launchArgs", secondInstanceArgs)
 }
 
 // startup is called when the app starts. The context is saved
--- main.go
+++ main.go
@@ -20,14 +20,20 @@ func main() {
 		Title:  "mdnotes",
 		Width:  1024,
 		Height: 768,
+		EnableDefaultContextMenu: false,
+		WindowStartState:   options.Maximised,
 		AssetServer: &assetserver.Options{
 			Assets: assets,
+		},
+		SingleInstanceLock: &options.SingleInstanceLock{
+			UniqueId:               "6060ee60-82be-4ad4-8ef9-24cc4ff5f5b4",
+			OnSecondInstanceLaunch: app.onSecondInstanceLaunch,
 		},
 		BackgroundColour: &options.RGBA{R: 27, G: 38, B: 54, A: 1},
 		OnStartup:        app.startup,
 		Bind: []interface{}{
 			app,
-		},
+		},		
 	})
 
 	if err != nil {
